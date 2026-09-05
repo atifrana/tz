ea8c17176a336a8eab58b6a5a11b7a5e6d7482ff
unknown <atifrana@gmail.com>
2026-09-06 03:19:32 +1000

    Release r11 rollback to stable search filtering

--- service/service.js
+++ service/service.js
@@ -32,7 +32,7 @@ const apps = {
                     [
                         new tizen.ApplicationControlData("module", [JSON.stringify(
                             {
-                                moduleName: 'atifrana/tz@r9',
+                                moduleName: 'atifrana/tz@r11',
                                 moduleType: 'gh',
                                 args: launchData
                             }
